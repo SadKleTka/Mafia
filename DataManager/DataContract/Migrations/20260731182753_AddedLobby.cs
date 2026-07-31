8381b51ff82643593fb8fe7f615d24c348158122
SadKleTka <kadinovaman321123@gmail.com>
2026-08-01 00:28:50 +0600

    addedLobby

---
+++ DataManager/DataContract/Migrations/20260731182753_AddedLobby.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DbContext.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedLobby : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Lobby",
+                columns: table => new
+                {
+                    LobbyId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Lobby", x => x.LobbyId);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Lobby_Name",
+                table: "Lobby",
+                column: "Name",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Lobby");
+        }
+    }
+}
