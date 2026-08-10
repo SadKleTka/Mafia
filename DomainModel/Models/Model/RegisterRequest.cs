4589701ac36cec54c0a08d22fdfb0e881eebd28e
SadKleTka <kadinovaman321123@gmail.com>
2026-08-10 22:10:47 +0600

    добавил папки для фронтенда

--- DomainModel/Models/Model/RegisterRequest.cs
+++ DomainModel/Models/Model/RegisterRequest.cs
@@ -1,10 +1,13 @@
-﻿namespace DomainModel.Models.Model;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DomainModel.Models.Model;
 
 /// <summary>
 /// Модель для регистрации пользователя
 /// </summary>
 public record RegisterRequest(
     string Username,
+    [EmailAddress]
     string Email,
     string Password
 );
