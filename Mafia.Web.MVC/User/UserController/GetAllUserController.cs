0d6eceb8bb5e594a28544b2b74ca17ded8272962
hashpolly <ivan@example.com>
2026-08-11 17:39:35 +0600

    Controller and service for getting all users

---
+++ Mafia.Web.MVC/User/UserController/GetAllUserController.cs
@@ -0,0 +1,25 @@
+﻿using DomainModel.Models.Entity;
+using Microsoft.AspNetCore.Mvc;
+using Service.Common.UserService;
+namespace Mafia.Web.MVC.UserController;
+
+[ApiController]
+[Route("[controller]")]
+
+public class GetAllUserController
+{
+    private readonly UserService _userService;
+
+    public GetAllUserController(UserService userService)
+    {
+        _userService = userService;
+    }
+    
+    [HttpGet]
+    [Route("getUsers")]
+    public async Task<IEnumerable<User>> getAllUsers()
+    {
+        var allUsers = await _userService.GetAllUsers();
+        return (allUsers);
+    }
+}
---
+++ Service/Common/User/UserService.cs
@@ -0,0 +1,22 @@
+﻿using DataManager.DataContract;
+using DomainModel.Models.Entity;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Service.Common.UserService;
+
+public class UserService
+{
+    private readonly AppDbContext _context;
+    public UserService(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<User>> GetAllUsers()
+    {
+        var users = await _context.Users.AsNoTracking().ToListAsync();
+       
+        return users;
+    }
+}
