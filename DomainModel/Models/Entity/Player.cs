4bd795585a981b2213c0fae729a86c0bb80e16ac
hashpolly <ivan@example.com>
2026-09-17 17:39:27 +0600

    энтити плеера

---
+++ DomainModel/Models/Entity/Player.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Enum.Enums;
+
+namespace DomainModel.Models.Entity;
+
+/// <summary>
+/// Энтити игрока
+/// </summary>
+public class Player
+{
+    [Key]
+    public string Nickname { get; set; }
+    
+    public Guid PlayerId { get; set; }
+    
+    public GameRole Role { get; set; }
+}
---
+++ Enum/Enums/GameRole.cs
@@ -0,0 +1,14 @@
+﻿namespace Enum.Enums;
+/// <summary>
+/// Класс для ролей в игре 
+/// </summary>
+public enum GameRole
+{
+    Doctor, 
+    Sheriff,
+    Lover,
+    Village,
+    MafiaBoss,
+    MAfia,
+    Maniac
+}
