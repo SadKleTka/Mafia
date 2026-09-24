39ca469ed468321f0e9bcf3d57d0426346f06a33
hashpolly <ivan@example.com>
2026-09-24 17:04:11 +0600

    Added Player

--- DataManager/DataContract/AppDbContext.cs
+++ DataManager/DataContract/AppDbContext.cs
@@ -14,6 +14,8 @@ public class AppDbContext : Microsoft.EntityFrameworkCore.DbContext
     public DbSet<User> Users { get; set; }
     
     public DbSet<Lobby> Lobbies { get; set; }
+    
+    public DbSet<Player> Players { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
