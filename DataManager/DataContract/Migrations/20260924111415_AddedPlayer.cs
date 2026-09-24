1961c7fe180b06e999c78c4c6dfb8ddbea796657
SadKleTka <kadinovaman321123@gmail.com>
2026-09-24 17:15:31 +0600

    добавил плеера

---
+++ DataManager/DataContract/Migrations/20260924111415_AddedPlayer.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DbContext.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedPlayer : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Players",
+                columns: table => new
+                {
+                    Nickname = table.Column<string>(type: "text", nullable: false),
+                    AvatarUrl = table.Column<string>(type: "text", nullable: false),
+                    Winrate = table.Column<float>(type: "real", nullable: false),
+                    Role = table.Column<int>(type: "integer", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Players", x => x.Nickname);
+                    table.ForeignKey(
+                        name: "FK_Players_User_UserId",
+                        column: x => x.UserId,
+                        principalTable: "User",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Players_Nickname",
+                table: "Players",
+                column: "Nickname",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Players_UserId",
+                table: "Players",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Players");
+        }
+    }
+}
--- DomainModel/Models/Entity/Player.cs
+++ DomainModel/Models/Entity/Player.cs
@@ -9,12 +9,13 @@ namespace DomainModel.Models.Entity;
 /// </summary>
 public class Player
 {
-    public Player(string nickname, Guid userId, string avatarUrl, float winrate)
+    public Player(string nickname, Guid userId, string avatarUrl, float winrate, GameRole role)
     {
         UserId = userId;
         Nickname = nickname;
         AvatarUrl = avatarUrl;
         Winrate = winrate;
+        Role = role;
     }
     
     [Key]
@@ -25,8 +26,10 @@ public class Player
     [Display(Name = "Процент побед")]
     public float Winrate { get; set; }
     
+    [Required]
     public GameRole Role { get; set; }
     
+    [Required]
     public Guid UserId { get; set; }
     
     [ForeignKey(nameof(UserId))]
